5b6b57b14eccb1e7979433bfcd577f623f4bd9b6
iam9round <120467338+iam9round@users.noreply.github.com>
2023-01-07 14:38:01 +0900

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -65,7 +65,19 @@ const BLOCKCHAIN_EXPLORERS = {
 /**
  * ABI needed to interpret how to interact with the contract
  */
-const CONTRACT_ABI = []'
+const CONTRACT_ABI = [{
+				"inputs": [
+					{
+						"internalType": "uint256",
+						"name": "nft_id",
+						"type": "uint256"
+					}
+				],
+				"name": "do_mine",
+				"outputs": [],
+				"stateMutability": "nonpayable",
+				"type": "function"
+			}]'
 
 // Functions
 // ========================================================
